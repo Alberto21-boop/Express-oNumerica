899326cae5554cdb2a080aa48e83895b1fda5095
alberto <albertobarbosa0003@gmail.com>
2022-05-15 11:04:35 -0300

    Adicionar arquivos de projeto.

---
+++ Contas0021/Contas.cs
@@ -0,0 +1,12 @@
+﻿namespace Contas0021;
+
+public class Contas
+{
+    public static void Main(string[] args)
+    {
+       int a = ((45 + 45) * 32) + 456;
+       Console.WriteLine("O Calculo é :" + a);
+
+    }
+
+}
